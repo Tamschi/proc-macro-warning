126e1ffba725e0071d770e4a15ebb342f38cc6ea
Tamme Schichler <tamme@schichler.dev>
2020-12-19 06:02:34 +0100

    This may work

--- src/lib.rs
+++ src/lib.rs
@@ -31,15 +31,15 @@ thread_local! {
 	static WARNINGS: Mutex<Vec<(Span, TokenStream)>> = Mutex::default();
 }
 
-#[cfg(all(feature = "nightly", proc_macro))]
+#[cfg(all(feature = "nightly", not(test)))]
 extern crate proc_macro;
 
-#[cfg(all(feature = "nightly", proc_macro))]
+#[cfg(all(feature = "nightly", not(test)))]
 pub fn warn(span: Span, message: &str) {
 	proc_macro::Diagnostic::spanned(span.unwrap(), proc_macro::Level::Warning, message).emit()
 }
 
-#[cfg(any(not(feature = "nightly"), not(proc_macro)))]
+#[cfg(any(not(feature = "nightly"), test))]
 pub fn warn(span: Span, message: &str) {
 	use proc_macro2::Literal;
 
